599a46d06711124e0b8c20cfe1a892ee92dcbe0d
christian <christian.margreitter@boku.ac.at>
2015-08-06 18:03:32 +0200

    clusters added

--- MDplot/R/MDplot.R
+++ MDplot/R/MDplot.R
@@ -161,3 +161,25 @@ MDplot_DSSP_timeseries <- function( VEC_timeseries )
 {
   
 }
+
+MDplot_load_clusters <- function( STRING_path )
+{
+  MAT_pre <- as.matrix( read.table( STRING_path ) )[ , -1  ]
+  MAT_pre <- MAT_pre[ , ( ( ncol( MAT_pre ) / 2 ) + 1 ):ncol( MAT_pre ) ]
+  return( MAT_pre )
+}
+
+MDplot_clusters <- function( MAT_clusters, INT_maximum_number = 0 )
+{
+  if( INT_maximum_number != 0 )
+  {
+    MAT_clusters <- MAT_clusters[ 1:INT_maximum_number, ]
+  }
+  MAT_clusters <- t( MAT_clusters )
+  PALETTE_clusters <- colorRampPalette( rev( brewer.pal( 11, 'Spectral' ) ) )
+  COLOURS_CLUSTERS <- PALETTE_clusters( nrow( MAT_clusters ) )
+  names( MAT_clusters ) <- 1:nrow( MAT_clusters )
+  barplot( MAT_clusters, col = COLOURS_CLUSTERS )#, xaxt = "n" )
+  #axis( 1, at = 1:6, labels = 1:nrow( MAT_clusters ), cex.axis = 1 )
+  legend( "topright", legend = 1:nrow( MAT_clusters ), col = COLOURS_CLUSTERS, pch = 19, cex = 1.25 )
+}
