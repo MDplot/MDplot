694ff0722ae7546cfa7412e8307da599b770f2a6
christian <christian.margreitter@boku.ac.at>
2015-09-07 15:28:34 +0200

    MDplot_TIcurve(): finished

--- MDplot/R/MDplot.R
+++ MDplot/R/MDplot.R
@@ -17,21 +17,48 @@ integrate_curve <- function( MAT_input )
     stop( paste( "Error: Number of columns in matrix not 2 or higher, but ",
                  ncol( MAT_input ), "!" ) )
   }
+  REAL_error <- NA
+  if( ncol( MAT_input ) > 2 )
+  {
+    REAL_error <- 0.0
+  }
   REAL_integral <- 0.0
   for( i in 2:nrow( MAT_input ) )
   {
-    REAL_addition <- ( ( MAT_input[ i, 1 ] - MAT_input[ i - 1, 1 ] ) *
+    REAL_integral <- REAL_integral + ( ( MAT_input[ i, 1 ] - MAT_input[ i - 1, 1 ] ) *
                      ( MAT_input[ i, 2 ] + MAT_input[ i - 1, 2 ] ) ) / 2
-    REAL_integral <- REAL_integral + REAL_addition
+    if( ncol( MAT_input ) > 2 )
+    {
+      REAL_error <- REAL_error + ( ( MAT_input[ i, 1 ] - MAT_input[ i - 1, 1 ] ) *
+                                     ( MAT_input[ i, 3 ] + MAT_input[ i - 1, 3 ] ) ) / 2
+    }
   }
-  return( list( integral = REAL_integral ) )
+  return( list( integral = REAL_integral, error = REAL_error ) )
+}
+
+MDplot_load_TIcurve <- function( STRING_path )
+{
+  return( as.matrix( read.table( STRING_path ) ) )
+}
+
+plot_segments <- function( MAT_values, VEC_spread, REAL_difference = 0.1 )
+{
+  par( new = TRUE )
+  segments( MAT_values[ , 1 ], MAT_values[ , 2 ] - VEC_spread, MAT_values[ , 1 ],
+            MAT_values[ , 2 ] + VEC_spread )
+  segments( MAT_values[ , 1 ] - REAL_difference, MAT_values[ , 2 ] - VEC_spread,
+            MAT_values[ , 1 ] + REAL_difference, MAT_values[ , 2 ] - VEC_spread )
+  segments( MAT_values[ , 1 ] - REAL_difference, MAT_values[ , 2 ] + VEC_spread,
+            MAT_values[ , 1 ] + REAL_difference, MAT_values[ , 2 ] + VEC_spread )
 }
 
 MDplot_TIcurve <- function( MAT_input, STRING_main_title = "Thermodynamic integration" )
 {
   par( oma = c( 0.35, 1.75, 0.45, 0.0 ) )
-  plot( MAT_input, ylim = c( min( MAT_input[ , 2 ] ), max( MAT_input[ , 2 ] ) ), xaxs = "i",
-        pch = 19, cex = 1.45, xlab = "", ylab = "", main = STRING_main_title )
+  TIplot <- plot( MAT_input, ylim = c( min( MAT_input[ , 2 ] ), max( MAT_input[ , 2 ] ) ),
+                  xaxs = "i", pch = 19, cex = 1.00, xlab = "", ylab = "",
+                  main = STRING_main_title )
+  plot_segments( MAT_input[ , 1:2 ], MAT_input[ , 3 ], 0.01 )
   mtext( side = 1, text = expression( lambda ), line = 3, cex = 1.45 )
   mtext( side = 2, 
          text = expression( frac( paste( partialdiff, "H" ), paste( partialdiff, lambda ) ) ), 
@@ -39,10 +66,12 @@ MDplot_TIcurve <- function( MAT_input, STRING_main_title = "Thermodynamic integr
   par( new = TRUE )
   plot( MAT_input, ylim = c( min( MAT_input[ , 2 ] ), max( MAT_input[ , 2 ] ) ), xaxs = "i",
         type = "l", lwd = 1, xaxt = "n", yaxt = "n", xlab = "", ylab = "" )
-  REAL_integral <- unlist( integrate_curve( MAT_input )[ "integral" ] )
+  REAL_integral <- round( unlist( integrate_curve( MAT_input )[ "integral" ] ), 2 )
+  REAL_error <- round( unlist( integrate_curve( MAT_input )[ "error" ] ), 2 )
   mtext( side = 1, line = 3.0, cex = 1.25,
          adj = 1,
-         text = substitute( paste( Delta, "G = ", REAL_integral, " [kJ/mol]" ) ) )
+         text = substitute( paste( Delta, "G = ", REAL_integral, " \u00b1 ", REAL_error,
+                                   " [kJ/mol]" ) ) )
   abline( h = 0, lwd = 1, lty = 3 )
 }
 
