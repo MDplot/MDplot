48a1cb3f80e98c3270da23a5aa0c0aa683a42c63
assurbanipal <sophiekrecht@hotmail.com>
2015-09-21 23:54:18 +0200

    --- filler ---

--- MDplot/tests/MDplot_install.R
+++ MDplot/tests/MDplot_install.R
@@ -1,10 +1,6 @@
 install.packages( "./MDplot_1.0.tar.gz", dependencies = TRUE,
                   repos = NULL, type = "source", INSTALL_opts = c( "--no-lock" ) )
 if( require( MDplot ) )
-{
   print( "Installation of MDplot version 1.0 succeeded!" )
-}
-else
-{
+if( !require( MDplot ) )
   print( "Installation of MDplot could not be completed successfully!" )
-}
