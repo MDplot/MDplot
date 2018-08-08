d8b1400835483182082da7054b1d707a801a3dda
Christian Margreitter <christian.margreitter@gmail.com>
2018-08-08 15:11:18 +0100

    improved "tessellation.R"

--- MDplot/R/tessellation.R
+++ MDplot/R/tessellation.R
@@ -7,8 +7,9 @@ plotMembrane <- function( inputResult,
                           plotPoints = TRUE,
                           plotEdges = TRUE,
                           plotCurvature = TRUE,
-                          colPoints = c( "red", "blue" ),
-                          colEdges = c( "red", "blue" ),
+                          curvatureType = "unsigned",
+                          colPoints = list( by = "surface", '1' = "red", '2' = "blue" ),
+                          colEdges = list( by = "surface", '1' = "red", '2' = "blue" ),
                           paletteCurvature = NA,
                           paletteRange = NA,
                           PNGsnapshot = NA,
@@ -23,7 +24,7 @@ plotMembrane <- function( inputResult,
     selectSnapshot <- 1
   if( is.na( paletteCurvature ) )
     paletteCurvature <- colorRampPalette( c( "white", "blue", "orange", "red", "black" ) )
-  if( is.na( paletteRange ) )
+  if( all( is.na( paletteRange ) ) )
     paletteRange <- range( as.vector( unlist( lapply( X = inputResult, FUN = function( surface ) lapply( X = surface, FUN = function( x ) x$localCurvature ) ) ) ) )
   if( length( paletteRange ) != 2 )
     stop( "The range of the pallette must be a vector of length two exactly." )
@@ -33,9 +34,13 @@ plotMembrane <- function( inputResult,
     stop( "In order to plot the edges, the element \"edgeIndices\" must be part of the input (see parameter \"verbose\" in the analysis function of the MANTICORE package)." )
   if( plotCurvature && is.null( inputResult[[ 1 ]][[ paste0( "surface_", surfaces[ 1 ] ) ]]$localCurvature ) )
     stop( "In order to plot the curvature as the colour of the tiles, the element \"localCurvature\" must be part of the result input (see parameter of the same name in the analysis function of the MANTICORE package)." )
-  if( ( plotPoints && length( colPoints ) < length( surfaces ) ) ||
-      ( plotEdges && length( colEdges ) < length( surfaces ) ) )
-    stop( "The colour vectors specified must at least be of the length of the \"surfaces\" vector." )
+  if( !is.list( colPoints ) || !is.list( colEdges ) || !( colPoints$by %in% c( "surface", "molecule" ) ) || !( colEdges$by %in% c( "surface" ) ) )
+    stop( "Parameters \"colPoints\" and \"colEdges\" must be lists with certain elements, please consult the manual." )
+  if( ( plotPoints && colPoints$by == "surface" && length( colPoints ) - 1 < length( surfaces ) ) ||
+      ( plotEdges && colEdges$by == "surface" && length( colEdges ) - 1  < length( surfaces ) ) )
+    stop( "If the colouring lists are set to hold information by surface, colours for every surface have to be specified." )
+  if( is.na( curvatureType ) || is.null( curvatureType ) || !( curvatureType %in% c( "unsigned", "signed" ) ) )
+    stop( "Parameter \"curvatureType\" must be either \"unsigned\" or \"signed\"." )
   if( any( is.na( appearance ) ) )
     appearance <- list( userMatrix = matrix( c(  0.6380738,  0.6212445, -0.4548815, 0,
                                                 -0.2128165,  0.7100536,  0.6712176, 0,
@@ -57,16 +62,35 @@ plotMembrane <- function( inputResult,
     for( iSurface in 1:length( surfaces ) )
     {
       referencePointsDF <- inputResult[[ selectSnapshot ]]$referencePointsDF
-      plot3d( x = referencePointsDF[ referencePointsDF[ , "clusterNumber" ] == surfaces[ iSurface ], "refPointX" ],
-              y = referencePointsDF[ referencePointsDF[ , "clusterNumber" ] == surfaces[ iSurface ], "refPointY" ],
-              z = referencePointsDF[ referencePointsDF[ , "clusterNumber" ] == surfaces[ iSurface ], "refPointZ" ],
-              type = 's',
-              size = 0.45,
-              add = TRUE,
-              xlim = range( referencePointsDF[ , "refPointX" ] ),
-              ylim = range( referencePointsDF[ , "refPointY" ] ),
-              zlim = range( referencePointsDF[ , "refPointZ" ] ),
-              col = colPoints[ surfaces[ iSurface ] ] )
+      listReferencePointsDF <- list()
+      colVector <- c()
+      if( colPoints$by == "surface" ) {
+        colVector <- colPoints[[ as.character( surfaces[ iSurface ] ) ]]
+        listReferencePointsDF[[ 1 ]] <- referencePointsDF
+      } else {
+        allMolecules <- unique( referencePointsDF$moleculeType )
+        for( iMolecule in 1:length( allMolecules ) )
+        {
+          listReferencePointsDF[[ iMolecule ]] <- referencePointsDF[ referencePointsDF$moleculeType == allMolecules[ iMolecule ], , drop = FALSE ]
+          colVector <- c( colVector, colPoints[[ allMolecules[ iMolecule ] ]] )
+        }
+      }
+      for( iSurfacePart in 1:length( listReferencePointsDF ) )
+      {
+        referencePointsDF <- listReferencePointsDF[[ iSurfacePart ]]
+        if( nrow( referencePointsDF ) == 0 )
+          next
+        plot3d( x = referencePointsDF[ referencePointsDF[ , "clusterNumber" ] == surfaces[ iSurface ], "refPointX" ],
+                y = referencePointsDF[ referencePointsDF[ , "clusterNumber" ] == surfaces[ iSurface ], "refPointY" ],
+                z = referencePointsDF[ referencePointsDF[ , "clusterNumber" ] == surfaces[ iSurface ], "refPointZ" ],
+                type = 's',
+                size = 0.525,
+                add = TRUE,
+                xlim = range( referencePointsDF[ , "refPointX" ] ),
+                ylim = range( referencePointsDF[ , "refPointY" ] ),
+                zlim = range( referencePointsDF[ , "refPointZ" ] ),
+                col = colVector[ iSurfacePart ] )
+      }
     }
 
   # plot the edges
@@ -74,6 +98,9 @@ plotMembrane <- function( inputResult,
     for( iSurface in 1:length( surfaces ) )
     {
       referencePointsDF <- inputResult[[ selectSnapshot ]]$referencePointsDF
+      colVector <- NULL
+      if( colEdges$by == "surface" )
+        colVector <- colEdges[[ as.character( surfaces[ iSurface ] ) ]]
       edgeIndices <- inputResult[[ selectSnapshot ]][[ paste0( "surface_", surfaces[ iSurface ] ) ]]$edgeIndices
       matEdges <- getEdgeCoordinates( referencePointsDF = referencePointsDF[ referencePointsDF[ , "clusterNumber" ] == surfaces[ iSurface ], ],
                                       edgeIndices = edgeIndices )
@@ -89,36 +116,42 @@ plotMembrane <- function( inputResult,
       segments3d( x = xVec,
                   y = yVec,
                   z = zVec,
-                  col = colEdges[ iSurface ] )
+                  lwd = 1.25,
+                  col = colVector )
     }
 
   # plot the triangles
   curColIndex <- 1
-  for( iSurface in 1:length( surfaces ) )
-  {
-    matTriangleIndices <- inputResult[[ selectSnapshot ]][[ paste0( "surface_", surfaces[ iSurface ] ) ]]$triangleIndices
-    vecLocalCurvature <- inputResult[[ selectSnapshot ]][[ paste0( "surface_", surfaces[ iSurface ] ) ]]$localCurvature
-    matTriangleCoordinates <- getCoordinatesFromPointIDs( referencePointsDF = inputResult[[ selectSnapshot ]]$referencePointsDF,
-                                                          indices = matTriangleIndices[ , c( "ind1", "ind2", "ind3" ), drop = FALSE ] )
-    colRange <- paletteCurvature( 10 )[ as.numeric( cut( c( paletteRange, vecLocalCurvature ), breaks = 10 ) ) ]
-    colRange <- colRange[ c( -2, -1 ) ]
-    vecColours <- c()
-    xVec <- c()
-    yVec <- c()
-    zVec <- c()
-    for( iTriangle in 1:nrow( matTriangleCoordinates ) )
+  if( plotCurvature )
+    for( iSurface in 1:length( surfaces ) )
     {
-      xVec <- c( xVec, matTriangleCoordinates[ iTriangle, c( "refPointX_1", "refPointX_2", "refPointX_3" ), drop = TRUE ] )
-      yVec <- c( yVec, matTriangleCoordinates[ iTriangle, c( "refPointY_1", "refPointY_2", "refPointY_3" ), drop = TRUE ] )
-      zVec <- c( zVec, matTriangleCoordinates[ iTriangle, c( "refPointZ_1", "refPointZ_2", "refPointZ_3" ), drop = TRUE ] )
-      vecColours <- c( vecColours, rep( colRange[ iTriangle ], times = 3 ) )
+      matTriangleIndices <- inputResult[[ selectSnapshot ]][[ paste0( "surface_", surfaces[ iSurface ] ) ]]$triangleIndices
+      vecLocalCurvature <- NULL
+      if( curvatureType == "unsigned" )
+        vecLocalCurvature <- inputResult[[ selectSnapshot ]][[ paste0( "surface_", surfaces[ iSurface ] ) ]]$localCurvature
+      else
+        vecLocalCurvature <- inputResult[[ selectSnapshot ]][[ paste0( "surface_", surfaces[ iSurface ] ) ]]$signedLocalCurvature
+      matTriangleCoordinates <- getCoordinatesFromPointIDs( referencePointsDF = inputResult[[ selectSnapshot ]]$referencePointsDF,
+                                                            indices = matTriangleIndices[ , c( "ind1", "ind2", "ind3" ), drop = FALSE ] )
+      colRange <- paletteCurvature( 10 )[ as.numeric( cut( c( paletteRange, vecLocalCurvature ), breaks = 10 ) ) ]
+      colRange <- colRange[ c( -2, -1 ) ]
+      vecColours <- c()
+      xVec <- c()
+      yVec <- c()
+      zVec <- c()
+      for( iTriangle in 1:nrow( matTriangleCoordinates ) )
+      {
+        xVec <- c( xVec, matTriangleCoordinates[ iTriangle, c( "refPointX_1", "refPointX_2", "refPointX_3" ), drop = TRUE ] )
+        yVec <- c( yVec, matTriangleCoordinates[ iTriangle, c( "refPointY_1", "refPointY_2", "refPointY_3" ), drop = TRUE ] )
+        zVec <- c( zVec, matTriangleCoordinates[ iTriangle, c( "refPointZ_1", "refPointZ_2", "refPointZ_3" ), drop = TRUE ] )
+        vecColours <- c( vecColours, rep( colRange[ iTriangle ], times = 3 ) )
+      }
+      triangles3d( x = xVec,
+                   y = yVec,
+                   z = zVec,
+                   col = vecColours )
+      curColIndex <- curColIndex + nrow( matTriangleCoordinates )
     }
-    triangles3d( x = xVec,
-                 y = yVec,
-                 z = zVec,
-                 col = vecColours )
-    curColIndex <- curColIndex + nrow( matTriangleCoordinates )
-  }
 
   # do the rotation stuff, if required
   if( interactive )
@@ -188,11 +221,12 @@ shootMovieMembrane <- function( inputResult, outputMovie, interactive = TRUE, sn
 
   # generate the movie
   system2( command = "ffmpeg",
-           args = c( "-framerate 4",
+           args = c( "-framerate 12",
                      paste0( "-i ", tempDir, "/snapshot_%04d.png" ),
                      "-c:v libx264",
                      "-r 24",
                      "-pix_fmt yuv420p",
+                     "-vf \"scale=trunc(iw/2)*2:trunc(ih/2)*2\"",
                      outputMovie ),
            input = 'y',
            wait = TRUE )
